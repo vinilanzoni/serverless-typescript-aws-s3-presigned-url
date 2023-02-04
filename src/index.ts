3d53c231201b327b25f00220568b0d6415a6afe4
Vinicius Bruno Lanzoni <>
2023-02-04 00:51:20 -0300

    Release

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import serverless from "serverless-http";
+import express, { Request, Response } from 'express';
+import { addAsync } from '@awaitjs/express';
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+
+const app = addAsync(express());
+
+const client = new S3Client({ 
+  region: "us-east-1",
+});
+
+async function getSignedFileUrl(fileName: string, bucket: string, expiresIn: number) {
+  // Instantiate the GetObject command,
+  // a.k.a. specific the bucket and key
+  const command = new PutObjectCommand({
+    Bucket: bucket,
+    Key: fileName,
+  });
+
+  // await the signed URL and return it
+  return await getSignedUrl(client, command, { expiresIn });
+}
+
+app.getAsync('/', async (req: Request, res: Response) => {
+  const presignedUrl = await getSignedFileUrl("cover.image", "2021-public-bucket", 3600);
+  res.send({ presignedUrl });
+});
+
+module.exports.handler = serverless(app);
